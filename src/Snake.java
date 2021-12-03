ddd59605258c958e8a80ee75e4d03e296837cf6c
Nandita64 <nanditasingh64@gmail.com>
2021-12-03 22:52:28 +0530

    Snake game

---
+++ src/Snake.java
@@ -0,0 +1,28 @@
+import javax.swing.JFrame;
+import java.awt.EventQueue;
+public class Snake extends JFrame {
+    public Snake() {
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        add(new Board());
+
+        setResizable(false);
+        pack();
+
+        setTitle("Snake");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+
+        EventQueue.invokeLater(() -> {
+            JFrame ex = new Snake();
+            ex.setVisible(true);
+        });
+    }
+}
